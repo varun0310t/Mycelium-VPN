e70b567190a1a6b74b888df8241e627afc48c802
varun0310t <chauhanvarun10th03@gmail.com>
2025-09-08 21:36:28 +0530

    feat: implement TUN interface creation with WinTun support

--- internal/tunnel/tunnelManager.go
+++ internal/tunnel/tunnelManager.go
@@ -1,1 +1,27 @@
 package tunnel
+
+import (
+	"log"
+
+	"golang.zx2c4.com/wireguard/tun"
+)
+
+func CreateTUN() tun.Device {
+	device, err := tun.CreateTUN("VPN-Interface", 1420)
+
+	if err != nil {
+		log.Printf("Failed to create TUN interface: %v", err)
+		log.Println("Hint: Ensure wintun.dll is in the same directory as the executable.")
+		return nil
+	}
+
+	name, err := device.Name()
+	if err != nil {
+		log.Printf("Failed to get device name: %v", err)
+		device.Close()
+		return nil
+	}
+
+	log.Printf("TUN Interface created: %s", name)
+	return device
+}
